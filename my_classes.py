ffeaf47c760d29035b1be919c67c6a693cfbc936
juliusmayr <mj4127@mci4me.at>
2025-04-10 17:27:18 +0200

    Noch nicht fertig

--- my_classes.py
+++ my_classes.py
@@ -1,20 +1,23 @@
 from my_functions import estimate_max_hr
 
-class Subject():
-    def __init__(self, first_name, last, sex, age):
+class Person():
+    def __init__(self, first_name, last_name):
         self.first_name = first_name
-        self.last_name = last
+        self.last_name = last_name
+
+class Subject(Person):
+    def __init__(self, first_name, last, sex, age):
+        super().__init__(first_name, last)
         self.sex = sex  
-        self.age = age 
+        self.__age = age 
 
     def estimate_max_hr(self, sex, age):
       """A function that estimates the maximum heart rate of a subject"""
       return estimate_max_hr(sex, age)
 
-class Supervisor():
+class Supervisor(Person):
     def __init__(self, first_name, last_name):
-        self.first_name = first_name
-        self.last_name = last_name
+        super().__init__(first_name, last_name)
 
 class Experiment():
     def __init__(self, name, date):
